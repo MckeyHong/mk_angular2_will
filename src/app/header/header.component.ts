4954e7443c000efd89b9ccfa8b890aee0f237c29
MckeyHong <pnv200414@hotmail.com>
2016-08-01 13:18:21 +0800

    測驗：透過Enter確認欄位輸入，避免使用雙向繫結更新目標屬性

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,8 @@ export class HeaderComponent implements OnInit {
   title = '<i>Angular2 練習</i>';
   titlelink = 'http://blog.miniasp.com/';
 
+  editTitleText = this.title;
+
   num = 1;
 
   constructor() { }
@@ -19,6 +21,12 @@ export class HeaderComponent implements OnInit {
 
   addNumber() {
     this.num = this.num + 1;
+  }
+
+  editTitle($event: KeyboardEvent) {
+    if ($event.keyCode == 13) {
+      this.title = this.editTitleText;
+    }
   }
 
 }
